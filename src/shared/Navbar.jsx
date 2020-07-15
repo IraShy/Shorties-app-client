48d4ce42103ff209205e0ee30f3b77006f885b0d
Iryna Shymbor <i.shymbor@gmail.com>
2020-07-15 14:55:57 +1000

    added a simple navbar

---
+++ src/shared/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/search">Search</Link>
+      <Link to="/notes/create">Add a note</Link>
+    </nav>
+  );
+};
+
+export default Navbar;
