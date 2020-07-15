d08a86a3359ff9c89e4656ed77b6ad8cf91e83ea
Iryna Shymbor <i.shymbor@gmail.com>
2020-07-15 22:34:29 +1000

    pre-deployment

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Navbar from "./../shared/Navbar";
+import Notes from "./Notes";
+// import Note from "./Note";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Switch>
+        <Route exact path="/notes" component={Notes} />
+        {/* <Route exact path="/notes/:id" component={Note} /> */}
+      </Switch>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Notes.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+class Notes extends React.Component {
+
+
+  render() {
+    return (<p>on Notes</p>)
+  }
+}
+
+export default Notes;
