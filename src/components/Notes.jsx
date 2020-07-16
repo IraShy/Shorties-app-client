c3f9aff081117587a274b5e086271e9ba192fcbe
Iryna Shymbor <i.shymbor@gmail.com>
2020-07-16 10:53:34 +1000

    notes component

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -2,10 +2,50 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 class Notes extends React.Component {
+  state = { notes: [] };
 
+  getNotes = async () => {
+    const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/notes`);
+    const data = await response.json();
+    this.setState({ notes: data });
+  };
+
+  deleteNote = async (id) => {
+    await fetch(`${process.env.REACT_APP_BACKEND_URL}/notes/${id}`, {
+      method: "DELETE",
+    });
+    this.getNotes();
+  };
+
+  renderNotes = () => {
+    return this.state.notes.map((note, index) => {
+      return (
+        <div key={index}>
+          <h1>{note.title}</h1>
+          <p>{note.body}</p>
+          <Link
+            to={{
+              pathname: `/notes/${note.id}`,
+              state: note,
+            }}
+          >
+            <button>View note</button>
+          </Link>
+
+          <button onClick={() => this.deleteNote(note.id)}>Delete</button>
+
+          <hr />
+        </div>
+      );
+    });
+  };
+
+  componentDidMount() {
+    this.getNotes();
+  }
 
   render() {
-    return (<p>on Notes</p>)
+    return this.renderNotes();
   }
 }
 
