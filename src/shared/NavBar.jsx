644636bc42f556e3f81bcf3947b9fbd366e31223
Ji Shan <uug54332017@gmail.com>
2020-07-17 21:14:07 +1000

    installed font-awesome and updated navbar icons

--- src/shared/NavBar.jsx
+++ src/shared/NavBar.jsx
@@ -5,8 +5,6 @@ import Nav from "react-bootstrap/Nav";
 import Form from "react-bootstrap/Form";
 import FormControl from "react-bootstrap/FormControl";
 import Button from "react-bootstrap/Button";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 const NavBar = () => {
   return (
