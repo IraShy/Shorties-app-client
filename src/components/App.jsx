4bad29f4993c1bae23ad0a1524e92d9fc1f63be8
Iryna Shymbor <i.shymbor@gmail.com>
2020-07-17 13:42:04 +1000

    login component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Navbar from "../shared/Navbar";
 import Notes from "./Notes";
+import Login from "./Login";
+import SignUp from "./SignUp";
 // import Note from "./Note";
 
 const App = () => {
@@ -9,6 +11,8 @@ const App = () => {
     <>
       <Navbar />
       <Switch>
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/signup" component={SignUp} />
         <Route exact path="/notes" component={Notes} />
         {/* <Route exact path="/notes/:id" component={Note} /> */}
       </Switch>
