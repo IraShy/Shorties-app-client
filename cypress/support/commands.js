52c9b2b7bbbe11670a68c84a96f1e5e264260ff8
Iryna Shymbor <i.shymbor@gmail.com>
2020-07-22 15:32:27 +1000

    login tests

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,1 +1,10 @@
 import '@testing-library/cypress/add-commands';
+
+Cypress.Commands.add('getUser', () => {
+  cy.fixture("user.json").then((user) => user)
+})
+
+Cypress.Commands.add("typeInLoginCredentials", (email, password) => {
+  cy.findByTestId("email").type(email);
+  cy.findByTestId("password").type(password);
+})
