ac30578f3406ffc2910ea5f6092b2be31e3beb24
Ji Shan <uug54332017@gmail.com>
2020-07-26 12:21:02 +1000

    extracted createNote fetch to client class

--- src/components/AddNote.jsx
+++ src/components/AddNote.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Context } from "../context/Context";
 import Joi from "joi-browser";
 import Validation from "../shared/Validation";
+import Client from "./Client"
 
 class AddNote extends Validation {
   static contextType = Context;
@@ -19,44 +20,16 @@ class AddNote extends Validation {
     picture: Joi.any(),
   });
 
+  client = new Client();
+
   onFormSubmit = async (event) => {
     event.preventDefault();
     const { note } = this.state;
     const { categories } = this.state;
-    const category_response = await fetch(
-      `${process.env.REACT_APP_BACKEND_URL}/categories`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${localStorage.getItem("token")}`,
-        },
-        body: JSON.stringify({ categories }),
-      }
-    );
 
-    const category_json = await category_response.json();
+    const createdNote = await this.client.createNote(note, categories);
 
-    // stringify to send "[]" to backend
-    note.category_ids = JSON.stringify(category_json.map((c) => c.id));
-
-    const data = new FormData();
-    for (let key in note) {
-      data.append(`note[${key}]`, note[key]);
-    }
-
-    const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/notes`, {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem("token")}`,
-      },
-      body: data,
-    });
-
-    const noteData = await response.json();
-    const noteToAdd = { ...noteData.note, picture: noteData.picture };
-
-    this.context.dispatchUser("add", noteToAdd);
+    this.context.dispatchUser("add", createdNote);
     this.props.history.push("/notes");
   };
 
